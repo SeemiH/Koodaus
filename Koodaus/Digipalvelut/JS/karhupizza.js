f588638d33cb2ede1a8b599052f18845e4b4e16c
SeemiH <38238770+SeemiH@users.noreply.github.com>
2018-04-10 09:09:02 +0300

    Add files via upload
    Kaikki

---
+++ Koodaus/Digipalvelut/JS/karhupizza.js
@@ -0,0 +1,22 @@
+/* onlick show/hide dropdown content*/
+function droppi() {
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+//Sulje dropdown klikkaamalla mualle
+window.onlick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classlist.contains('show')) {
+        openDropdown.classlist.remove('show');
+      }
+    }
+  }
+}
